31c4609b23ad4551ac8d6f18c41da2207704fef4
Justin Richards <justin.richards@msnst.com>
2014-08-01 09:17:44 -0500

    Empty cursor set as default.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QtQuick/QQuickItem>
-
+#include <QPixmap>
 #include "camerasource.h"
 
 int main(int argc, char *argv[])
@@ -10,6 +10,10 @@ int main(int argc, char *argv[])
 
     // Register our CameraSource so our QML code can use it
     qmlRegisterType<CameraSource>("CameraSource", 1, 0, "CameraSource");
+
+    QPixmap emptyCursor(16, 16);
+    emptyCursor.fill(Qt::transparent);
+    app.setOverrideCursor(QCursor(emptyCursor));
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
