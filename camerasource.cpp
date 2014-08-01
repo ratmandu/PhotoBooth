7339c7ca89063ba3fbba689246acc8e2cf97346a
Justin Richards <justin.richards@msnst.com>
2014-07-31 22:34:12 -0500

    Added a few comments

--- camerasource.cpp
+++ camerasource.cpp
@@ -3,8 +3,12 @@
 CameraSource::CameraSource(QObject *parent) :
   QObject(parent)
 {
+  // Create the camera object
   camera = new QCamera(this);
 
+  // Set the capture mode to StillImage
   camera->setCaptureMode(QCamera::CaptureStillImage);
+
+  // Start the camera
   camera->start();
 }
