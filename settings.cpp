1b2867df7047d4a8d605b7041af027b6cb2b5a17
Justin Richards <justin.richards@msnst.com>
2014-08-01 09:41:44 -0500

    Added code for QSettings to QML class

--- settings.cpp
+++ settings.cpp
@@ -3,4 +3,54 @@
 Settings::Settings(QObject *parent) :
   QObject(parent)
 {
+  m_settings = new QSettings();
+}
+
+Settings::~Settings()
+{
+  m_settings->deleteLater();
+}
+
+void Settings::setValue(const QString &key, const QVariant &value)
+{
+  m_settings->setValue(key, value);
+  m_settings->sync();
+}
+
+QVariant Settings::getValue(const QString &key, const QVariant &defaultValue) const
+{
+  return m_settings->value(key, defaultValue);
+}
+
+void Settings::setString(const QString &key, const QString &value)
+{
+  m_settings->setValue(key, value);
+  m_settings->sync();
+}
+
+QString Settings::getString(const QString &key, const QString &defaultValue) const
+{
+  return m_settings->value(key, defaultValue).toString();
+}
+
+void Settings::setBool(const QString &key, const bool &value)
+{
+  m_settings->setValue(key, value);
+  m_settings->sync();
+}
+
+bool Settings::getBool(const QString &key, const bool &defaultValue) const
+{
+  return m_settings->value(key, defaultValue).toBool();
+}
+
+void Settings::setInt(const QString &key, const int &value)
+{
+  m_settings->setValue(key, value);
+  m_settings->sync();
+}
+
+int Settings::getInt(const QString &key, const int &defaultValue) const
+{
+  return m_settings->value(key, defaultValue).toInt();
 }
--- settings.h
+++ settings.h
@@ -2,12 +2,26 @@
 #define SETTINGS_H
 
 #include <QObject>
+#include <QSettings>
 
 class Settings : public QObject
 {
   Q_OBJECT
 public:
   explicit Settings(QObject *parent = 0);
+  ~Settings();
+
+  Q_INVOKABLE void setValue(const QString &key, const QVariant &value);
+  Q_INVOKABLE QVariant getValue(const QString &key, const QVariant &defaultValue = QVariant) const;
+  Q_INVOKABLE void setString(const QString &key, const QString &value);
+  Q_INVOKABLE QString getString(const QString &key, const QString &defaultValue) const;
+  Q_INVOKABLE void setBool(const QString &key, const bool &value);
+  Q_INVOKABLE bool getBool(const QString &key, const bool &defaultValue = false) const;
+  Q_INVOKABLE void setInt(const QString &key, const int &value);
+  Q_INVOKABLE int getInt(const QString &key, const int &defaultValue = 0) const;
+
+private:
+  QSettings *m_settings;
 
 signals:
 
